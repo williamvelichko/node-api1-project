23e74292fc9ea1879ed54bc089cac8c59d55de79
williamvelichko <williamvelichko2003@gmail.com>
2022-02-07 13:22:04 -0800

    im able to add get the specific user

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 // BUILD YOUR SERVER HERE
+const { json } = require("express");
 const express = require("express");
 
 const model = require("./users/model");
@@ -20,6 +21,40 @@ server.get("/api/users", (req, res) => {
     .catch(() => {
       res.status(500).json({ message: "could not get users" });
     });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  let { id } = req.params;
+  model
+    .findById(id)
+    .then((user) => {
+      if (user == null) {
+        res.status(404).json({ message: `couldnt find user with id ${id}` });
+      } else {
+        res.json(user);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "could not get user" });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  let body = req.body;
+  if (!body.name) {
+    res.status(500).json({ message: `needs a name` });
+  } else if (!body.bio) {
+    res.status(500).json({ message: `needs a bio` });
+  } else {
+    model
+      .insert(body)
+      .then((newUser) => {
+        res.json(newUser);
+      })
+      .catch(() => {
+        res.status(500).json({ message: "could not get NewUser" });
+      });
+  }
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
