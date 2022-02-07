15b360207d17f206e345426d7d8e67e429c9bd42
williamvelichko <williamvelichko2003@gmail.com>
2022-02-07 12:49:30 -0800

    im able to get the users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,25 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const model = require("./users/model");
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  console.log("server working");
+  res.json("its working");
+});
+
+server.get("/api/users", (req, res) => {
+  model
+    .find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "could not get users" });
+    });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 9000;
 
 // START YOUR SERVER HERE
+console.log("hello William");
+
+server.listen(port, () => {
+  console.log("server has started");
+});
